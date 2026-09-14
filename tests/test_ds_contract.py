43092b61ec8fb4e385d1986f7aef5ca7c47694e6
Ânderson Q <anquadros@gmail.com>
2026-09-14 13:18:45 -0300

    chore: sync design system 2.4.3 provenance

--- tests/test_ds_contract.py
+++ tests/test_ds_contract.py
@@ -13,8 +13,8 @@ WEB = ROOT / "web"
 SRC = WEB / "src"
 UI = SRC / "components" / "ui"
 DS_RELEASE = "2.4.3"
-DS_COMMIT = "df92a5786b3a61f3c4295b052f05a7e8a0b91f44"
-DS_SOURCE_COMMIT = "d4f6f4c"
+DS_COMMIT = "7d801cc03c1ff06cfc094f239e1d44a0e3c33dfc"
+DS_SOURCE_COMMIT = "df92a57"
 
 RAW_CONTROL = re.compile(r"<(button|input|select|textarea)(?:\s|>|/)")
 ARBITRARY_VALUE = re.compile(r"\b[a-z][a-z-]*-\[[^\]]+\]")
